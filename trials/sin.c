d064b472f8635b4cf3f506656f945d911eb787d1
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-10-17 16:30:19 +0530

    efficiency increased

--- trials/sin.c
+++ trials/sin.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
-#include <math.h>
-
-float fact(float x){
-    float f=1.0;
-    for (; x > 0; x-=1) {
-        f *= x;
-    }
-    return f;
-}
 
 int main() {
-    float sinval=0.0, angle;
+    float sinval=0.0, term, angle;
     int i;
 
     printf("fact(5) = %f\n", fact(5.0));
@@ -20,10 +11,13 @@ int main() {
     scanf("%f", &angle);
     angle = angle * 3.14159 / 180.0;
     printf("Angle in radian is : %f \n", angle);
-
+    
+    term = angle;
+    sinval += term;
 
     for (i = 1; i < 10; i++) {
-        sinval += pow(-1.0, i-1.0) * pow(angle, 2.0*i - 1.0) / fact(2.0*i-1.0);
+        term = term * (-1) * angle * angle / ((2 * i) * (2 * i + 1));
+        sinval += term;
     }
 
     printf("The Sin of %f is %f \n", angle, sinval);
